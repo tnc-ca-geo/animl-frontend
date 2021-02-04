306da72362ca53f51d6056cf7fc1971c4998bd4c
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2021-02-04 13:10:27 -0800

    Refactor bounding box component

--- src/features/detailsModal/BoundingBox.js
+++ src/features/detailsModal/BoundingBox.js
@@ -1,10 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { styled } from '../../theme/stitches.config';
-import ResizableRect from 'react-resizable-rotatable-draggable';
+import Draggable from 'react-draggable';
+import { ResizableBox } from 'react-resizable';
+import 'react-resizable/css/styles.css';
 
-// TODO: this doesn't work. Figure out how to style
-const StyledResizableRect = styled(ResizableRect, {
-  border: '$2 solid $blue400',
+
+const ResizeHandle = styled('div', {
+  width: '$3',
+  height: '$3',
+  display: 'inline-block',
+  position: 'absolute',
+  variants: {
+    location: {
+      sw: {
+        bottom: '0',
+        left: '0',
+        cursor: 'sw-resize',
+      },
+      se: {
+        bottom: '0',
+        right: '0',
+        cursor: 'se-resize'
+      },
+      nw: {
+        top: '0',
+        left: '0',
+        cursor: 'nw-resize',
+      }, 
+      ne: {
+        top: '0',
+        right: '0',
+        cursor: 'ne-resize',
+      }
+    }
+  }
+});
+
+const DragHandle = styled('div', {
+  margin: '$3',
+  width: 'calc(100% - 32px)', // stitches bug w/ parsing variables in calc
+  height: 'calc(100% - 32px)',
+  ':hover': {
+    cursor: 'grab',
+  },
+  ':user-select': {
+    cursor: 'grabbing',
+  }
+});
+
+const StyledResizableBox = styled(ResizableBox, {
+  boxSizing: 'border-box',
+  position: 'absolute !important;',
+  border: '2px solid #345EFF',
 });
 
 // convert [left, top, width, height] in absolute values to 
@@ -14,8 +61,8 @@ const absToRel = (rect, image) => {
   const { imageWidth, imageHeight } = image;
   const ymin = Math.round(top) / imageHeight;
   const xmin = Math.round(left) / imageWidth;
-  const ymax = Math.round(top) + Math.round(height) / imageHeight;
-  const xmax = Math.round(left) + Math.round(width) / imageWidth;
+  const ymax = (Math.round(top) + Math.round(height)) / imageHeight;
+  const xmax = (Math.round(left) + Math.round(width)) / imageWidth;
   return [ymin, xmin, ymax, xmax];
 };
 
@@ -35,25 +82,11 @@ const BoundingBox = ({ imageWidth, imageHeight, initialBbox }) => {
   // [ymin, xmin, ymax, xmax] in relative values
   // so we are using that format in state. 
   const [ bbox, setBbox ] = useState(initialBbox);
-  const { left, top, width, height } = relToAbs(bbox, imageWidth, imageHeight)
+  let { left, top, width, height } = relToAbs(bbox, imageWidth, imageHeight);
+  const [ constraintX, setConstraintX ] = useState(Infinity);
+  const [ constraintY, setConstraintY ] = useState(Infinity);
 
-  useEffect(() => {
-    console.log('creating new bbox: ', bbox);
-  }, []);
-  // TODO: prevent bbox from moving off image
-
-  const handleResize = (style, isShiftKey, type) => {
-    const rect = style;
-    const image = { imageWidth, imageHeight };
-    const newBbox = absToRel(rect, image);
-    setBbox(newBbox);
-  };
-
-  const handleDrag = (deltaX, deltaY) => {
-    console.log('dragging')
-    console.log('deltax: ', deltaX)
-    console.log('deltay: ', deltaY)
-
+  const onDrag = (event, {deltaX, deltaY}) => {
     const rect = {
       left: left + deltaX,
       top: top + deltaY,
@@ -65,29 +98,82 @@ const BoundingBox = ({ imageWidth, imageHeight, initialBbox }) => {
     setBbox(newBbox);
   };
 
-  const handleMoveEnd = () => {
-    console.log('bounding box adjusted');
-  };
+  const onResize = (event, { size, handle }) => {
+      // drags from left or top handles need require repositioning the box.
+      // bottom & right can be left alone
+    if (['n', 'w', 'ne', 'nw', 'sw'].includes(handle)) {
+      if (handle.indexOf('n') > -1) {
+        const deltaHeight = size.height - height;
+        top -= deltaHeight;
+      }
+      if (handle.indexOf('w') > -1) {
+        const deltaWidth = size.width - width;
+        left -= deltaWidth;
+      }
+    }
+
+    // Prevent box from going out of bounds
+    if (handle.indexOf('e') > -1) {
+      const right = imageWidth - size.width - left;  
+      if (right <= 0) {
+        console.log('right out of bounds')
+        setConstraintX(width);
+      }
+    }
+    if (handle.indexOf('w') > -1) {
+      if (left <= 0) {
+        console.log('left out of bounds')
+        setConstraintX(width);
+      }
+    }
+    if (handle.indexOf('n') > -1) {
+      if (top <= 0) {
+        console.log('top out of bounds');
+        setConstraintY(height);
+      }
+    }
+    if (handle.indexOf('s') > -1) {
+      const bottom = imageHeight - size.height - top;  
+      if (bottom <= 0) {
+        console.log('bottom out of bounds');
+        setConstraintY(height);
+      }
+    }
+    
+    width = size.width;
+    height = size.height;
+    const rect = {left, top, width, height};
+    const image = { imageWidth, imageHeight };
+    const newBbox = absToRel(rect, image);
+    setBbox(newBbox);
+  }
+
+  const onResizeStop = () => {
+    setConstraintX(Infinity);
+    setConstraintY(Infinity);
+    console.log('setting max constraints to infinity')
+  }
 
   return (
-    <StyledResizableRect
-      left={left}
-      top={top}
-      width={width}
-      height={height}
-      zoomable='n, w, s, e, nw, ne, se, sw'
-      rotatable={false}
-      onResize={handleResize}
-      onResizeEnd={handleMoveEnd}
-      onDrag={handleDrag}
-      onDragEnd={handleMoveEnd}
-      // onResizeStart={this.handleResizeStart}
-      // onDragStart={this.handleDragStart}
-      // minWidth={10}
-      // minHeight={10}
-    />
-  );
-
+    <Draggable
+      bounds='parent'
+      handle='.drag-handle'
+      position={{x: left, y: top}}
+      onDrag={onDrag}
+    >
+      <StyledResizableBox
+        width={width}
+        height={height}
+        maxConstraints={[constraintX, constraintY]}
+        resizeHandles={['sw', 'se', 'nw', 'ne']}
+        handle={(location) => <ResizeHandle location={location} />}
+        onResize={onResize}
+        onResizeStop={onResizeStop}
+      >
+        <DragHandle className='drag-handle'/>
+      </StyledResizableBox>
+    </Draggable>
+  )
 };
 
 export default BoundingBox;
