e79d0f8bf8481ede6603a136d9cd26b59aba8ba7
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2021-02-11 07:54:36 -0800

    Style automation rules list

--- src/features/filters/AutomationRulesList.js
+++ src/features/filters/AutomationRulesList.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { styled } from '../../theme/stitches.config.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import IconButton from '../../components/IconButton';
 import SubmitButton from '../../components/SubmitButton';
 
 
-const RuleTitle = styled.div({
-
-});
 
 const FieldRow = styled.div({
   paddingBottom: '$3',
@@ -23,42 +22,76 @@ const ButtonRow = styled(FieldRow, {
   }
 });
 
+const Rule = styled('li', {
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+const RuleTitle = styled.div({
+  fontWeight: '$4',
+});
+
+const RulesList = styled('ul', {
+  listSyle: 'none',
+  padding: '0',
+  marginTop: '0',
+});
+
+const StyledRuleDescription = styled.div({
+  fontSize: '$3',
+  p: {
+    color: '$gray600',
+    marginTop: '$1',
+  }
+})
+
 const RuleDescription = ({ rule, models }) => {
   const model = models.find((model) => model._id === rule.action.model);
 
   return (
-    <div>
-      {rule.event.type === 'image-added' && 
-        `When an image is added, `
-      }
-      {rule.event.type === 'label-added' && 
-        `When an ${rule.event.label} is detected, `
-      }
-      {rule.action.type === 'run-inference'
-        ? `submit the image to ${model.name} ${model.version} for inference.`
-        : `send an alert to ${rule.action.alertRecipient}.`
-      }
-    </div>
+    <StyledRuleDescription>
+      <RuleTitle>{rule.name}</RuleTitle>
+      <p>
+        {rule.event.type === 'image-added' && 
+          `When an image is added, `
+        }
+        {rule.event.type === 'label-added' && 
+          `When an ${rule.event.label} is detected, `
+        }
+        {rule.action.type === 'run-inference'
+          ? `submit the image to ${model.name} ${model.version} for inference.`
+          : `send an alert to ${rule.action.alertRecipient}.`
+        }
+      </p>
+    </StyledRuleDescription>
   );
 };
 
 
 const AutomationRulesList = ({ view, models, onAddRuleClick }) => {
-  console.log('view: ', view);
-  console.log('models: ', models);
+  const handleRuleDeleteClick = (e) => {
+    console.log('rule to delete: ', e.target.dataset.rule);
+  };
+
   return (
     <div>
-      <div>Active Rules</div>
-      <ul>
+      <RulesList>
         {view.automationRules.map((rule) => {
           return (
-            <li>
-              <RuleTitle>{rule.name}</RuleTitle>
+            <Rule>
               <RuleDescription rule={rule} models={models} />
-            </li>
+              <IconButton
+                variant='ghost'
+                disabled={view && !view.editable}
+                data-rule={rule}
+                onClick={handleRuleDeleteClick}
+              >
+                <FontAwesomeIcon icon={['fas', 'trash-alt']} />
+              </IconButton>
+            </Rule>
           )
         })}
-      </ul>
+      </RulesList>
       <ButtonRow>
         <SubmitButton
           size='large'
