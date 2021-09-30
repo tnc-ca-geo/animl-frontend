62374a40ec55d083a9173864f6c46b093987a381
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2021-09-30 09:29:19 -0700

    Create script and env variables for staging build

--- src/config.js
+++ src/config.js
@@ -1,16 +1,21 @@
 export const DATE_FORMAT_EXIF = 'YYYY:MM:DD hh:mm:ss';
 export const DATE_FORMAT_READABLE = 'YYYY-MM-DD hh:mm:ss';
 
+console.log('process.env: ', process.env);
+const stage = process.env.REACT_APP_STAGE || process.env.NODE_ENV;
+
 const API_URLS = {
   development: 'https://qxtg8jjpqf.execute-api.us-west-2.amazonaws.com/dev/external/',
+  staging: 'https://qxtg8jjpqf.execute-api.us-west-2.amazonaws.com/dev/external/',
   production: 'TODO: add production URL'
 };
-export const API_URL = API_URLS[process.env.NODE_ENV];
+export const API_URL = API_URLS[stage];
 
 const IMAGES_URLS = {
-  development: 'https://d3ps2k4314vkoe.cloudfront.net/',
-  production: 'TODO: add production URL',
+  devevelopment: 'https://d3ps2k4314vkoe.cloudfront.net/',
+  staging: 'https://d3ps2k4314vkoe.cloudfront.net/',
+  production: 'TODO: add production URL'
 };
-export const IMAGES_URL = IMAGES_URLS[process.env.NODE_ENV];
+export const IMAGES_URL = IMAGES_URLS[stage];
 
 export const IMAGE_QUERY_LIMITS = [10, 50, 100];
