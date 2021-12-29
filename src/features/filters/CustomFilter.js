e9c5a9f5fff795e04c9506c9b3e98c9fb0a93045
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2021-12-29 09:53:49 -0600

    Add form for custom filter

--- src/features/filters/CustomFilter.js
+++ src/features/filters/CustomFilter.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { styled } from '../../theme/stitches.config.js';
 import { useDispatch } from 'react-redux'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import Accordion from '../../components/Accordion';
+import { FormWrapper, FieldRow, ButtonRow } from '../../components/Form';
+import Button from '../../components/Button';
+
+const StyledFormWrapper = styled(FormWrapper, {
+  display: 'flex',
+  width: '45px'
+});
+
+const StyledButtonRow = styled(ButtonRow, {
+  paddingBottom: '$0'
+})
 
 const CustomFilter = () => {
   const dispatch = useDispatch();
@@ -14,12 +27,47 @@ const CustomFilter = () => {
   //   dispatch(checkboxFilterToggled(payload));
   // };
 
+  const customFilterSchema = Yup.object().shape({
+    filter: Yup.string().required('A view ID is required'),
+  });
+
+  const handleCustomFilterSubmit = (values) => {
+    console.log('applying custom filter: ', values)
+    // dispatch(editView('delete', values));
+    // setQueuedForClose(true);
+  };
+
   return (
     <Accordion
       label='Custom'
       expandedDefault={false}
     >
-      custom filter input
+      <div>
+        <StyledFormWrapper>
+          <Formik
+            initialValues={{ filter: '' }}
+            validationSchema={customFilterSchema}
+            onSubmit={(values) => handleCustomFilterSubmit(values)}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <FieldRow>
+                  <Field
+                    name='filter'
+                    id='filter'
+                    component='textarea'
+                  />
+                </FieldRow>
+                <StyledButtonRow>
+                  <Button type='submit' size='small'>
+                    Apply
+                  </Button>
+                </StyledButtonRow>
+              </Form>
+            )}
+          </Formik>
+        </StyledFormWrapper>
+      </div>
     </Accordion>
   );
 };
