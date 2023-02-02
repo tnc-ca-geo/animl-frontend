6a570c0754d6c2f5b519f625b95472170bc70557
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2023-02-02 08:59:42 -0800

    allow project members to export data

--- src/auth/roles.js
+++ src/auth/roles.js
@@ -4,13 +4,13 @@ const OBSERVER = 'project_observer';
 
 export const WRITE_OBJECTS_ROLES =              [MANAGER, MEMBER];
 export const WRITE_VIEWS_ROLES =                [MANAGER, MEMBER];
+export const EXPORT_DATA_ROLES =                [MANAGER, MEMBER];
 export const WRITE_IMAGES_ROLES =               [MANAGER];
 export const WRITE_DEPLOYMENTS_ROLES =          [MANAGER];
 export const WRITE_AUTOMATION_RULES_ROLES =     [MANAGER];
 export const WRITE_CAMERA_REGISTRATION_ROLES =  [MANAGER];
 export const QUERY_WITH_CUSTOM_FILTER =         [MANAGER];
 export const READ_STATS_ROLES =                 [MANAGER];
-export const EXPORT_DATA_ROLES =                [MANAGER];
 
 export const hasRole = (currRoles, targetRoles = []) => (
   currRoles && currRoles.some((role) => targetRoles.includes(role))
