038f51458f69747da1a63ac2fbdc25d00724dc70
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2023-07-26 16:05:38 -0700

    make label cells scrollable in images table, #56

--- src/features/images/ImagesTable.jsx
+++ src/features/images/ImagesTable.jsx
@@ -126,6 +126,12 @@ const DataCell = styled(TableCell, {
         //   paddingLeft: '12px',
         // },
       }
+    },
+    scrollable: {
+      true: {
+        overflowY: 'scroll',
+        alignItems: 'start',
+      }
     }
   }
 });
@@ -396,16 +402,18 @@ const ImagesTable = ({ workingImages, hasNext, loadNextPage }) => {
             onClick={(e) => handleRowClick(e, row.id)}
             selected={focusIndex.image === index}
           >
-            {row.cells.map(cell => {
-              return (
-                <DataCell
-                  {...cell.getCellProps()}
-                  selected={focusIndex.image === index}
-                >
-                  {cell.render('Cell')}
-                </DataCell>
-              )
-            })}
+            {row.cells.map(cell => (
+              <DataCell
+                {...cell.getCellProps()}
+                selected={focusIndex.image === index}
+                scrollable={
+                  cell.column.Header === 'Labels' && 
+                  cell.value.props.objects.length > 3
+                }
+              >
+                {cell.render('Cell')}
+              </DataCell>
+            ))}
           </TableRow>
         );
       }
