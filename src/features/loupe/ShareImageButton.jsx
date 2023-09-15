b397e6ae553777cc2a6dd5b9fd256872fc9bb914
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2023-09-15 06:53:20 -0700

    Fix share url bug, #142

--- src/features/loupe/ShareImageButton.jsx
+++ src/features/loupe/ShareImageButton.jsx
@@ -57,7 +57,7 @@ const ShareImageButton = ({ imageId }) => {
       style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}
     >
       <StyledLabel htmlFor="shareURL">Share: </StyledLabel>
-      <StyledURLInput type="text" id="shareURL" readonly="readonly" defaultValue={shareURL} />
+      <StyledURLInput type="text" id="shareURL" readonly="readonly" value={shareURL} />
       <Tooltip>
         <TooltipTrigger asChild>
           <IconButton 
