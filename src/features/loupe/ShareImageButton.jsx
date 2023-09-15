c0f55d0914e8b64832c74a1c8ccadc0e335ed3df
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2023-09-15 07:18:05 -0700

    Fix share url typo

--- src/features/loupe/ShareImageButton.jsx
+++ src/features/loupe/ShareImageButton.jsx
@@ -42,7 +42,7 @@ const StyledURLInput = styled('input', {
 const ShareImageButton = ({ imageId }) => {
   const selectedProject = useSelector(selectSelectedProject);
   const allImagesView = selectedProject.views.find((v) => v.name === 'All images');
-  const shareURL = `${window.location.origin}/${selectedProject._id}/${allImagesView._id}?img=${imageId}`;
+  const shareURL = `${window.location.origin}/app/${selectedProject._id}/${allImagesView._id}?img=${imageId}`;
   const dispatch = useDispatch();
 
   const [ showToast, setShowToast ] = useState(false);
