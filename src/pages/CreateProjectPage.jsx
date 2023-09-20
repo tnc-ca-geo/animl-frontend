d1f47b2e1ff214772b0f7bd2a064f60620dd6b7f
Oliver Roick <oliver@roick.email>
2023-09-20 12:22:10 +0200

    Hide create project page if not superuser

--- src/pages/CreateProjectPage.jsx
+++ src/pages/CreateProjectPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import { Page } from '../components/Page.jsx';
+import { NotFound } from '../components/NotFound.jsx'
+import { selectUserUsername, selectUserAuthStatus, selectUserIsSuperUser } from '../features/user/userSlice.js';
+import LoginForm from '../features/user/LoginForm.jsx';
 
 const CreateProjectPage = () => {
+  const authStatus = useSelector(selectUserAuthStatus);
+  const user = useSelector(selectUserUsername);
+  const isSuperUser = useSelector(selectUserIsSuperUser);
+  const signedIn = authStatus === 'authenticated' && user;
+
+  if (!signedIn) {
+    return <Page><LoginForm /></Page>;
+  }
+
   return (
     <Page>
-      <h1>Create Project</h1>
+      {isSuperUser ? <h1>Create Project</h1> : <NotFound />}
     </Page>
   )
 }
