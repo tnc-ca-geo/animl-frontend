d2738427f4d3225a10fa85e2c48a32542c132f85
Oliver Roick <oliver@roick.email>
2023-09-20 11:32:48 +0200

    Add create-project page

---
+++ src/pages/CreateProjectPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Page } from '../components/Page.jsx';
+
+const CreateProjectPage = () => {
+  return (
+    <Page>
+      <h1>Create Project</h1>
+    </Page>
+  )
+}
+
+export default CreateProjectPage;
