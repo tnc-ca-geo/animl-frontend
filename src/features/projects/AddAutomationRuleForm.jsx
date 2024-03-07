4e8b44707166bb6c7ffbc2cdccf854d25dba0f08
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2024-03-07 10:38:35 -0800

    Hide 'empty' category config in automation rule form

--- src/features/projects/AddAutomationRuleForm.jsx
+++ src/features/projects/AddAutomationRuleForm.jsx
@@ -7,15 +7,8 @@ import * as Yup from 'yup';
 import { updateAutomationRules, selectMLModels, fetchModels } from './projectsSlice.js';
 import SelectField from '../../components/SelectField.jsx';
 import Button from '../../components/Button.jsx';
-import {
-  FormWrapper,
-  FieldRow,
-  ButtonRow,
-  FormFieldWrapper,
-  FormError
-} from '../../components/Form.jsx';
+import { FormWrapper, FieldRow, ButtonRow, FormFieldWrapper, FormError } from '../../components/Form.jsx';
 import CategoryConfigForm from './CategoryConfigForm.jsx';
-
 
 const CategoryConfigSection = styled('div', {
   maxHeight: '220px',
@@ -34,10 +27,7 @@ const emptyRule = {
 };
 
 const addRuleSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('A name is required'),
+  name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('A name is required'),
   event: Yup.object().shape({
     type: Yup.object().shape({
       label: Yup.string().required(),
@@ -46,7 +36,7 @@ const addRuleSchema = Yup.object().shape({
     label: Yup.string().when('type.value', {
       is: (val) => val === 'label-added',
       then: Yup.string().required('You must specify a label'),
-    })
+    }),
   }),
   action: Yup.object().shape({
     type: Yup.object().shape({
@@ -55,38 +45,36 @@ const addRuleSchema = Yup.object().shape({
     }),
     model: Yup.object().when('type.value', {
       is: (val) => val === 'run-inference',
-      then: Yup.object().shape({
-        label: Yup.string().required(),
-        value: Yup.string().required('You must specify a model'),
-      }).nullable(),
+      then: Yup.object()
+        .shape({
+          label: Yup.string().required(),
+          value: Yup.string().required('You must specify a model'),
+        })
+        .nullable(),
     }),
     alertRecipients: Yup.string().when('type.value', {
       is: (val) => val === 'send-alert',
       then: Yup.string()
         // .email('Email adderess is invalid')
-        .test(
-          'email-list-test',
-          'One or more email addresses were invalid', 
-          function (value) {
-            if (value) {
-              let schema = Yup.string().email();
-              const emails = value.replace(/\s/g, '').split(',');
-              let pass = true;
-              emails.forEach((email) => {
-                if (!schema.isValidSync(email)) { pass = false }
-              });
-              return pass;
-            }
-            else {
-              return false;
-            }
+        .test('email-list-test', 'One or more email addresses were invalid', function (value) {
+          if (value) {
+            let schema = Yup.string().email();
+            const emails = value.replace(/\s/g, '').split(',');
+            let pass = true;
+            emails.forEach((email) => {
+              if (!schema.isValidSync(email)) {
+                pass = false;
+              }
+            });
+            return pass;
+          } else {
+            return false;
           }
-        )
-        .required('One or more email addresses were invalid')
-    })
-  })
+        })
+        .required('One or more email addresses were invalid'),
+    }),
+  }),
 });
-
 
 const AddAutomationRuleForm = ({ project, availableModels, hideAddRuleForm, rule }) => {
   const dispatch = useDispatch();
@@ -95,7 +83,7 @@ const AddAutomationRuleForm = ({ project, availableModels, hideAddRuleForm, rule
   // fetch model source records
   useEffect(() => {
     if (!models || !models.length) {
-      dispatch(fetchModels({ _ids: availableModels}));
+      dispatch(fetchModels({ _ids: availableModels }));
     }
   }, [models, availableModels, dispatch]);
 
@@ -122,184 +110,145 @@ const AddAutomationRuleForm = ({ project, availableModels, hideAddRuleForm, rule
     <FormWrapper>
       <Formik
         initialValues={rule ? ruleToVals(rule) : { ...emptyRule }}
-        validationSchema={addRuleSchema} 
+        validationSchema={addRuleSchema}
         onSubmit={handleSaveRulesSubmit}
       >
-      {({ values, errors, touched, setFieldValue, setFieldTouched, 
-        isValid, dirty }) => (
-        <Form>
-
-          {/* name */}
-          <FieldRow>
-            <FormFieldWrapper>
-              <label htmlFor='rule-name'>Rule name</label>
-              <Field
-                name='name'
-                id='rule-name'
-                value={values.name}
-                placeholder='Rule name...'
-              />
-              <ErrorMessage
-                component={FormError}
-                name='name'
-              />
-            </FormFieldWrapper>
-          </FieldRow>
-
-          {/* trigger */}
-          <FieldRow>
-            <FormFieldWrapper>
-              <SelectField
-                name='event.type'
-                label='Trigger'
-                value={values.event.type}
-                onChange={setFieldValue}
-                onBlur={setFieldTouched}
-                error={_.has(errors, 'event.type.value') &&
-                  errors.event.type.value
-                }
-                touched={touched.event}
-                options={[
-                  { value: 'image-added', label: 'Image added' },
-                  { value: 'label-added', label: 'Label added' },
-                ]}
-                isSearchable={false}
-              />
-            </FormFieldWrapper>
-            {values.event.type.value === 'label-added' && (
-              <FormFieldWrapper css={{ flexGrow: '0' }}>
-                <label htmlFor='event-label'>Label</label>
-                <Field
-                  id='event-label'
-                  name='event.label'
-                  value={values.event.label ? values.event.label : '' }
-                />
-                <ErrorMessage
-                  component={FormError}
-                  name='event.label'
-                />
+        {({ values, errors, touched, setFieldValue, setFieldTouched, isValid, dirty }) => (
+          <Form>
+            {/* name */}
+            <FieldRow>
+              <FormFieldWrapper>
+                <label htmlFor="rule-name">Rule name</label>
+                <Field name="name" id="rule-name" value={values.name} placeholder="Rule name..." />
+                <ErrorMessage component={FormError} name="name" />
               </FormFieldWrapper>
-            )}
-          </FieldRow>
+            </FieldRow>
 
-          {/* action */}
-          <FieldRow>
-            <FormFieldWrapper>
-              <SelectField
-                name='action.type'
-                label='Action'
-                value={values.action.type}
-                onChange={(name, value) => {
-                  setFieldValue(name, value);
-                  if (value.value === 'send-alert') {
-                    setFieldValue('action.categoryConfig', {});
-                  } 
-                  else if (value.value === 'run-inference') {
-                    setFieldValue('action.model', null);
-                  }
-                }}
-                onBlur={setFieldTouched}
-                error={_.has(errors, 'action.type.value') &&
-                  errors.action.type.value
-                }
-                touched={touched.action}
-                options={[
-                  { value: 'run-inference', label: 'Run inference' },
-                  { value: 'send-alert', label: 'Send alert' },
-                ]}
-                isSearchable={false}
-              />
-            </FormFieldWrapper>
-            {values.action.type.value === 'run-inference' && (
+            {/* trigger */}
+            <FieldRow>
               <FormFieldWrapper>
                 <SelectField
-                  name='action.model'
-                  label='Model'
-                  value={values.action.model}
-                  onChange={(name, value) => {
-                    setFieldValue(name, value);
-                    const selectedModel = models.find((m) => (
-                      m._id === value.value
-                    ));
-                    const categoryConfig = {};
-                    selectedModel.categories.forEach((cat) => {
-                      categoryConfig[cat.name] = {
-                        confThreshold: selectedModel.defaultConfThreshold,
-                        disabled: false,
-                      };                      
-                    });
-                    setFieldValue('action.categoryConfig', categoryConfig);
-                  }}
+                  name="event.type"
+                  label="Trigger"
+                  value={values.event.type}
+                  onChange={setFieldValue}
                   onBlur={setFieldTouched}
-                  error={_.has(errors, 'action.model.value') &&
-                    errors.action.model.value
-                  }
-                  touched={touched.action}
-                  options={availableModels.map((model) => ({
-                    value: model,
-                    label: `${model}`,
-                  }))}
+                  error={_.has(errors, 'event.type.value') && errors.event.type.value}
+                  touched={touched.event}
+                  options={[
+                    { value: 'image-added', label: 'Image added' },
+                    { value: 'label-added', label: 'Label added' },
+                  ]}
                   isSearchable={false}
                 />
               </FormFieldWrapper>
-            )}
-            {values.action.type.value === 'send-alert' && (
-              <FormFieldWrapper css={{ minWidth: '300px' }}>
-                <label htmlFor='action-alert-recipients'>To</label>
-                <Field
-                  name='action.alertRecipients'
-                  id='action-alert-recipients'
-                  value={values.action.alertRecipients 
-                    ? values.action.alertRecipients 
-                    : ''
-                  }
-                  placeholder='Comma-separated list of email addresses...'
-                />
-                <ErrorMessage
-                  component={FormError}
-                  name='action.alertRecipients'
+              {values.event.type.value === 'label-added' && (
+                <FormFieldWrapper css={{ flexGrow: '0' }}>
+                  <label htmlFor="event-label">Label</label>
+                  <Field id="event-label" name="event.label" value={values.event.label ? values.event.label : ''} />
+                  <ErrorMessage component={FormError} name="event.label" />
+                </FormFieldWrapper>
+              )}
+            </FieldRow>
+
+            {/* action */}
+            <FieldRow>
+              <FormFieldWrapper>
+                <SelectField
+                  name="action.type"
+                  label="Action"
+                  value={values.action.type}
+                  onChange={(name, value) => {
+                    setFieldValue(name, value);
+                    if (value.value === 'send-alert') {
+                      setFieldValue('action.categoryConfig', {});
+                    } else if (value.value === 'run-inference') {
+                      setFieldValue('action.model', null);
+                    }
+                  }}
+                  onBlur={setFieldTouched}
+                  error={_.has(errors, 'action.type.value') && errors.action.type.value}
+                  touched={touched.action}
+                  options={[
+                    { value: 'run-inference', label: 'Run inference' },
+                    { value: 'send-alert', label: 'Send alert' },
+                  ]}
+                  isSearchable={false}
                 />
               </FormFieldWrapper>
-            )}
-          </FieldRow>
+              {values.action.type.value === 'run-inference' && (
+                <FormFieldWrapper>
+                  <SelectField
+                    name="action.model"
+                    label="Model"
+                    value={values.action.model}
+                    onChange={(name, value) => {
+                      setFieldValue(name, value);
+                      const selectedModel = models.find((m) => m._id === value.value);
+                      const categoryConfig = {};
+                      selectedModel.categories.forEach((cat) => {
+                        categoryConfig[cat.name] = {
+                          confThreshold: selectedModel.defaultConfThreshold,
+                          disabled: false,
+                        };
+                      });
+                      setFieldValue('action.categoryConfig', categoryConfig);
+                    }}
+                    onBlur={setFieldTouched}
+                    error={_.has(errors, 'action.model.value') && errors.action.model.value}
+                    touched={touched.action}
+                    options={availableModels.map((model) => ({
+                      value: model,
+                      label: `${model}`,
+                    }))}
+                    isSearchable={false}
+                  />
+                </FormFieldWrapper>
+              )}
+              {values.action.type.value === 'send-alert' && (
+                <FormFieldWrapper css={{ minWidth: '300px' }}>
+                  <label htmlFor="action-alert-recipients">To</label>
+                  <Field
+                    name="action.alertRecipients"
+                    id="action-alert-recipients"
+                    value={values.action.alertRecipients ? values.action.alertRecipients : ''}
+                    placeholder="Comma-separated list of email addresses..."
+                  />
+                  <ErrorMessage component={FormError} name="action.alertRecipients" />
+                </FormFieldWrapper>
+              )}
+            </FieldRow>
 
-          {/* category configurations */}
-          {values.action.categoryConfig && Object.entries(values.action.categoryConfig).length > 0 && 
-            <CategoryConfigSection>
-              <label>Confidence thresholds</label>
-              <FieldArray name='categoryConfigs'>
-                <>
-                  {Object.entries(values.action.categoryConfig).map(([k, v]) => (
-                    <CategoryConfigForm key={k} catName={k} config={v} />
-                  ))}
-                </>
-              </FieldArray>
-            </CategoryConfigSection>
-          }
-          
-          <ButtonRow>
-            <Button
-              type='button'
-              size='large'
-              onClick={handleDiscardRuleClick}
-            >
-              Cancel
-            </Button>
-            <Button
-              type='submit'
-              size='large'
-              disabled={!isValid || !dirty}
-            >
-              Save
-            </Button>
-          </ButtonRow>
-        </Form>
-      )}
+            {/* category configurations */}
+            {values.action.categoryConfig && Object.entries(values.action.categoryConfig).length > 0 && (
+              <CategoryConfigSection>
+                <label>Confidence thresholds</label>
+                <FieldArray name="categoryConfigs">
+                  <>
+                    {Object.entries(values.action.categoryConfig)
+                      .filter(([k]) => k !== 'empty') // NOTE: manually hiding "empty" categories b/c it isn't a real category returned by MDv5
+                      .map(([k, v]) => (
+                        <CategoryConfigForm key={k} catName={k} config={v} />
+                      ))}
+                  </>
+                </FieldArray>
+              </CategoryConfigSection>
+            )}
+
+            <ButtonRow>
+              <Button type="button" size="large" onClick={handleDiscardRuleClick}>
+                Cancel
+              </Button>
+              <Button type="submit" size="large" disabled={!isValid || !dirty}>
+                Save
+              </Button>
+            </ButtonRow>
+          </Form>
+        )}
       </Formik>
     </FormWrapper>
   );
 };
-
 
 // map from values to automation rule schema
 function valsToRule({ name, event, action }) {
@@ -315,8 +264,7 @@ function valsToRule({ name, event, action }) {
   if (action.type.value === 'run-inference') {
     newRule.action.mlModel = action.model.value;
     newRule.action.categoryConfig = action.categoryConfig;
-  }
-  else if (action.type.value === 'send-alert') {
+  } else if (action.type.value === 'send-alert') {
     const recipients = action.alertRecipients.replace(/\s/g, '').split(',');
     newRule.action.alertRecipients = recipients;
   }
@@ -339,30 +287,27 @@ function ruleToVals(rule) {
     event: {
       type: {
         value: rule.event.type,
-        label: mapValueToLabel[rule.event.type]
+        label: mapValueToLabel[rule.event.type],
       },
       ...(rule.event.label && { label: rule.event.label }),
     },
     action: {
       type: {
         value: rule.action.type,
-        label: mapValueToLabel[rule.action.type]
+        label: mapValueToLabel[rule.action.type],
       },
-    }
+    },
   };
 
   if (rule.action.type === 'run-inference') {
     vals.action.model = { value: rule.action.mlModel, label: rule.action.mlModel };
     vals.action.categoryConfig = rule.action.categoryConfig;
-  }
-  else if (rule.action.type === 'send-alert') {
+  } else if (rule.action.type === 'send-alert') {
     const recipients = rule.action.alertRecipients.join(', ');
     vals.action.alertRecipients = recipients;
   }
 
   return vals;
 }
-
-
 
 export default AddAutomationRuleForm;
