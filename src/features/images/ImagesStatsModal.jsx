ec05c772cb54c4d41f85dc47d967beffa093bd61
nevoniuk <nevoniuk@purdue.edu>
2024-03-25 18:18:23 -0700

    got rid of imports

--- src/features/images/ImagesStatsModal.jsx
+++ src/features/images/ImagesStatsModal.jsx
@@ -5,18 +5,66 @@ import { fetchStats, fetchTask, selectStatsLoading, selectImagesStats } from '..
 import { selectActiveFilters } from '../filters/filtersSlice.js';
 import { SimpleSpinner, SpinnerOverlay } from '../../components/Spinner';
 import NoneFoundAlert from '../../components/NoneFoundAlert';
+import Accordion from '../../components/Accordion';
+import {Label, SelectedCount, AccordionHeaderNoHover} from '../../components/Accordion.jsx';
+import { CheckboxLabel } from '../../components/CheckboxLabel.jsx';
 
-const StatsDisplay = styled('div', {
+const StatsInnerDisplay = styled('div', {
   border: '1px solid $border',
   maxHeight: '50vh',
   overflowY: 'scroll',
+
+});
+const StatsDisplay = styled('div', {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '5px',
+  userSelect: 'none',
+  touchAction: 'none'
 });
 
+
+const StatsItemNoList = ({label, count}) => {
+  return (
+    <AccordionHeaderNoHover>
+      <Label>{label}</Label>
+      <SelectedCount>{count}</SelectedCount>
+    </AccordionHeaderNoHover>
+  );
+};
+
+let mappings = {};
+mappings = {imageCount: "Image Count:", reviewedCount: "Reviewed Images:",
+  reviewerList: "Reviewer List:", notReviewed: "Not Reviewed:", labelList: "Label List:",
+  multiReviewerCount: "Multi Reviewer Count:", userId: "User:", reviewed: "Reviewed:"};
+function mapLabel(s) {
+  return (mappings[s] == undefined) ? (s + ":") : mappings[s];
+}
+const StatsItem = ({stat}) => {
+  let value = stat['value']
+  const condition = Number.isInteger(value)
+  return (
+    <>
+      {condition ? 
+      <StatsItemNoList label={stat['key']} count={value}/> :
+      <Accordion label={stat['key']} expandedDefault={false}>
+        {value.map((v, index) => (
+            <CheckboxLabel key={index} checked={true}
+            css={{ fontFamily: '$sourceSansPro' }}>{v['key']} {v['value']}</CheckboxLabel>
+        ))}
+      </Accordion>
+      }
+    </>
+  );
+};
+//will actually be a function that returns either a label or list if 
 const StyledStatsDisclaimer = styled('div', {
   paddingTop: 10,
   fontSize: '$3',
   color: '$textMedium',
 });
+
 
 const StatsDisclaimer = () => (
   <StyledStatsDisclaimer>
@@ -38,6 +86,8 @@ const StatsDisclaimer = () => (
   </StyledStatsDisclaimer>
 );
 
+
+
 const ImagesStatsModal = ({ open }) => {
   const dispatch = useDispatch();
   const filters = useSelector(selectActiveFilters);
@@ -50,6 +100,7 @@ const ImagesStatsModal = ({ open }) => {
     const noErrors = !errors || errors.length === 0;
     if (open && stats === null && !noneFound && !isLoading && noErrors) {
       dispatch(fetchStats(filters));
+      
     }
   }, [open, stats, imagesStatsLoading, filters, dispatch]);
 
@@ -59,7 +110,46 @@ const ImagesStatsModal = ({ open }) => {
       dispatch(fetchTask(imagesStatsLoading.taskId));
     }
   }, [getStatsPending, stats, dispatch]);
+  
+  
+  const Stats = [];
+  /** Parses JSON array */
+  function parseArray(value) {
+    let arr = []; let j = 0; let arraykey = "";
+    for (let k = 0; k < value.length; k++) {
+      let arrayval = "";
+      for (const [key, val] of Object.entries(value[k])) {
+        arraykey = (arraykey == "") ? (mapLabel(key)) : arraykey;
+        arrayval = (arrayval == "") ? (arrayval + val) : (arrayval + ", " + mapLabel(key) + " " + val);
+      }
+      arr[j++] = {"key": arraykey, "value": arrayval};
+    }
+    return arr;
+  }
+  /** Converts stats to key/value pairs*/
+  function ConvertStats(stats) {
+    let i = 0;
+    for (const [key, value] of Object.entries(stats)) {
+      if (Number.isInteger(value)) {
+        Stats[i++] = {"key": mapLabel(key), "value": value};
+      }
+      else {
+        if (Array.isArray(value)) {
+          Stats[i++] = {"key": mapLabel(key), "value": parseArray(value)}
+        } else {
+          let arr = []; let j = 0;
+          for (const [objkey, objval] of Object.entries(value)) {
+            arr[j++] = {"key": mapLabel(objkey), "value": objval}
+          }
+          Stats[i++] = {"key": mapLabel(key), "value": arr}
+        }
+      }
+    }
+  }
 
+  if (stats != null) {
+    ConvertStats(stats);
+  }
   return (
     <div>
       {imagesStatsLoading.isLoading && (
@@ -70,10 +160,14 @@ const ImagesStatsModal = ({ open }) => {
       {imagesStatsLoading.noneFound && (
         <NoneFoundAlert>We couldn&apos;t find any images that matched this set of filters.</NoneFoundAlert>
       )}
-      {stats && (
+      {stats && Stats && (Stats.length > 0) && (
         <>
           <StatsDisplay>
-            <pre>{JSON.stringify(stats, null, 2)}</pre>
+          <StatsInnerDisplay>
+            {Stats.map((stat, index) => (
+                    <StatsItem key={index} stat = {stat}/>
+                ))}
+          </StatsInnerDisplay>
           </StatsDisplay>
           <StatsDisclaimer />
         </>
@@ -81,5 +175,4 @@ const ImagesStatsModal = ({ open }) => {
     </div>
   );
 };
-
 export default ImagesStatsModal;
