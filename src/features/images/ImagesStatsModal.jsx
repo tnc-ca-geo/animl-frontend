712a62d850587c7c20504c2104b68c0a0f667c5b
nevoniuk <nevoniuk@purdue.edu>
2024-04-02 14:59:15 -0700

    Dashboard feature

--- src/features/images/ImagesStatsModal.jsx
+++ src/features/images/ImagesStatsModal.jsx
@@ -10,57 +10,88 @@ import {
 import { selectActiveFilters } from '../filters/filtersSlice.js';
 import { SimpleSpinner, SpinnerOverlay } from '../../components/Spinner';
 import NoneFoundAlert from '../../components/NoneFoundAlert';
-import Accordion from '../../components/Accordion';
-import {Label, SelectedCount, AccordionHeaderNoHover} from '../../components/Accordion.jsx';
-import { CheckboxLabel } from '../../components/CheckboxLabel.jsx';
-
-const StatsInnerDisplay = styled('div', {
+import {SelectedCount} from '../../components/Accordion.jsx';
+import { indigo } from '@radix-ui/colors';
+import {
+  ComposedChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
+const StatsDash = styled('div', {
   border: '1px solid $border',
-  maxHeight: '50vh',
-  overflowY: 'scroll',
-
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gridTemplateRows: '1fr 1fr 1fr',
+  background: '$backgroundDark',
+  borderRadius: '5px',
+  padding: '10px',
+  gap: '10px'
 });
-const StatsDisplay = styled('div', {
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '5px',
-  userSelect: 'none',
-  touchAction: 'none'
-});
-
-
-const StatsItemNoList = ({label, count}) => {
-  return (
-    <AccordionHeaderNoHover>
-      <Label>{label}</Label>
-      <SelectedCount>{count}</SelectedCount>
-    </AccordionHeaderNoHover>
-  );
-};
-
+const Label = styled('div', {
+  marginBottom: '20px'
+})
 let mappings = {};
-mappings = {imageCount: "Image Count:", reviewedCount: "Reviewed Count:",
-  reviewerList: "Reviewer List:", notReviewed: "Not Reviewed:", labelList: "Label List:",
-  multiReviewerCount: "Multi Reviewer Count:", userId: "User:", reviewed: "Reviewed:"};
+mappings = {imageCount: "Image Count", reviewedCount: "Reviewed Count",
+  reviewerList: "Reviewer List", notReviewed: "Not Reviewed", labelList: "Label List",
+  multiReviewerCount: "Multi Reviewer Count", userId: "User", reviewed: "Reviewed"};
+
 function mapLabel(s) {
   return (mappings[s] == undefined) ? (s + ":") : mappings[s];
 }
-const StatsItem = ({stat}) => {
-  let value = stat['value']
-  const condition = Number.isInteger(value)
+
+function changeCard(color, row, col, cspan, rspan) {
+  const StatsCard = styled('div', {
+    background: color,
+    padding: '15px',
+    borderRadius: '5px',
+    width: '100%',
+    height: '100%',
+    overflowX: 'scroll',
+    overflowY: 'scroll',
+    gridColumn: col + " / span " + cspan,
+    gridRow: row + " / span " + rspan,
+  });
+  return StatsCard;
+}
+
+
+const StatsItem = ({name, stat}) => {
+  switch (name) {
+    case "imageCount": {
+      return (
+        <RatioCard label={mapLabel(name)} tnum={stat} bnum={null} col={1} row={2} rspan={1} cspan={1}></RatioCard>
+      );
+    }
+    case "reviewedCount": {
+      return (
+        <RatioCard label={mapLabel(name)} tnum={stat['reviewed']} bnum={stat['notReviewed']}
+        col={1} row={1} span={1} rspan={1} cspan={1}></RatioCard>
+      );
+    }
+    case "reviewerList": {
+      return (
+        <ListCard label={mapLabel(name)} list={stat} col={1} row={3} rspan={1} cspan={2}></ListCard>
+      );
+    }
+    case "labelList": {
+      return (
+        <GraphCard label={mapLabel(name)} list={stat} col={2} row={1} rspan={2} cspan={2}></GraphCard>
+      );
+    }
+    case "multiReviewerCount": {
+      return (
+        <RatioCard label={mapLabel(name)} tnum={stat} bnum={null} col={3} row={3} rspan={1} cspan={1}></RatioCard>
+      );
+    }
+  }
   return (
-    <>
-      {condition ? 
-      <StatsItemNoList label={stat['key']} count={value}/> :
-      <Accordion label={stat['key']} expandedDefault={false}>
-        {value.map((v, index) => (
-            <CheckboxLabel key={index} checked={true}
-            css={{ fontFamily: '$sourceSansPro' }}>{v['key']} {v['value']}</CheckboxLabel>
-        ))}
-      </Accordion>
-      }
-    </>
+    <StyledStatsDisclaimer>
+      There is nothing to display for the statistic: {name}
+    </StyledStatsDisclaimer>
   );
 };
 
@@ -99,7 +130,121 @@ const StatsDisclaimer = () => (
   </StyledStatsDisclaimer>
 );
 
+const RatioCard = ({label, tnum, bnum, row, col, cspan, rspan}) => {
+  const StatsCard = changeCard('white', row, col, cspan, rspan)
+  const Container = styled('div', {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  })
+  const NumerContainer = styled('div', {
+    width: '100%',
+  })
+  const LargeNumber = styled(SelectedCount, {
+    fontSize: '30px',
+    width: '100%',
+    marginLeft: '5px',
+    
+  })
+  const SmallNumber = styled('div', {
+    width: '100%',
+    textAlign: 'right',
+    alignSelf: 'flex-end'
+  })
+  
+  return (
+    <StatsCard>
+      <Label>{label}</Label>
+      <Container>
+        <NumerContainer>
+          <LargeNumber>{tnum}</LargeNumber>
+        </NumerContainer>
+        {bnum !== null && 
+        <SmallNumber>/ {bnum}</SmallNumber>}
+      </Container>
+    </StatsCard>
+  );
+}
 
+const ListCard = ({label, list, row, col, cspan, rspan}) => {
+  const StatsCard = changeCard('white', row, col, cspan, rspan)
+  const Table = styled('div', {
+    maxWidth: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'scroll',
+    borderRadius: '5px',
+    border: '1px solid $border',
+    maxHeight: '102px'
+  });
+  const Row = styled('div', {
+    display: 'flex',
+    flexDirection: 'row',
+    maxHeight: '50px',
+  });
+  const TableCell = styled('div', {
+    borderBottom: '1px solid $border',
+    textAlign: 'left',
+    minWidth: '200px',
+    marginRight: '1px',
+    padding: '$2',
+    overflowX: 'scroll'
+  });
+  const Header = () => {
+    let headers = list[0]
+    return(
+       <Row>
+        {Object.keys(headers).map((keyName, i) => (
+            <TableCell key={i} css={{background: '$backgroundDark'}}>{mapLabel(keyName)}</TableCell>
+          ))
+          } 
+        </Row>
+    );
+  }
+  return (
+    <StatsCard>
+      <Label>{label}</Label>
+       <Table>
+       <Header/>
+        {list.map((stat, index) => (
+          <Row key={index}>
+            {Object.keys(stat).map((keyName, i) => (
+                <TableCell key={i}>{stat[keyName]}</TableCell>
+              ))
+            } 
+          </Row>
+        ))}
+       </Table>
+    </StatsCard>
+  );
+}
+
+const GraphCard = ({label, list, row, col, cspan, rspan}) => {
+  const StatsCard = changeCard('white', row, col, cspan, rspan)
+  const data = []
+  for (const [objkey, objval] of Object.entries(list)) {
+    data.push({"name": objkey, "Total Labels": objval})
+  }
+  return (
+    <StatsCard>
+      <Label>{label}</Label>
+        <ComposedChart
+          layout="vertical"
+          width={350}
+          height= {200}
+          data={data}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis type="number" />
+          <YAxis dataKey="name" type="category" scale="band" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Total Labels" barSize={20} fill={indigo.indigo11} />
+        </ComposedChart>
+    </StatsCard>
+    
+  );
+}
 
 const ImagesStatsModal = ({ open }) => {
   const dispatch = useDispatch();
@@ -117,52 +262,14 @@ const ImagesStatsModal = ({ open }) => {
     }
   }, [open, stats, imagesStatsLoading, filters, dispatch]);
 
+
   useEffect(() => {
     const getStatsPending = imagesStatsLoading.isLoading && imagesStatsLoading.taskId;
     if (getStatsPending) {
       dispatch(fetchTask(imagesStatsLoading.taskId));
     }
   }, [imagesStatsLoading, dispatch]);
-  
-  
-  const Stats = [];
-  /** Parses JSON array */
-  function parseArray(value) {
-    let arr = []; let j = 0; let arraykey = "";
-    for (let k = 0; k < value.length; k++) {
-      let arrayval = "";
-      for (const [key, val] of Object.entries(value[k])) {
-        arraykey = (arraykey == "") ? (mapLabel(key)) : arraykey;
-        arrayval = (arrayval == "") ? (arrayval + val) : (arrayval + ", " + mapLabel(key) + " " + val);
-      }
-      arr[j++] = {"key": arraykey, "value": arrayval};
-    }
-    return arr;
-  }
-  /** Converts stats to key/value pairs*/
-  function ConvertStats(stats) {
-    let i = 0;
-    for (const [key, value] of Object.entries(stats)) {
-      if (Number.isInteger(value)) {
-        Stats[i++] = {"key": mapLabel(key), "value": value};
-      }
-      else {
-        if (Array.isArray(value)) {
-          Stats[i++] = {"key": mapLabel(key), "value": parseArray(value)}
-        } else {
-          let arr = []; let j = 0;
-          for (const [objkey, objval] of Object.entries(value)) {
-            arr[j++] = {"key": mapLabel(objkey), "value": objval}
-          }
-          Stats[i++] = {"key": mapLabel(key), "value": arr}
-        }
-      }
-    }
-  }
 
-  if (stats != null) {
-    ConvertStats(stats);
-  }
   return (
     <div>
       {imagesStatsLoading.isLoading && (
@@ -175,19 +282,19 @@ const ImagesStatsModal = ({ open }) => {
           We couldn&apos;t find any images that matched this set of filters.
         </NoneFoundAlert>
       )}
-      {stats && Stats && (Stats.length > 0) && (
-        <>
-          <StatsDisplay>
-          <StatsInnerDisplay>
-            {Stats.map((stat, index) => (
-                    <StatsItem key={index} stat = {stat}/>
-                ))}
-          </StatsInnerDisplay>
-          </StatsDisplay>
-          <StatsDisclaimer />
-        </>
+      {stats && (
+        <div>
+           <StatsDash>
+              {Object.keys(stats).map((keyName, i) => (
+                <StatsItem key={i} name={keyName} stat={stats[keyName]}></StatsItem>
+                ))
+              }
+          </StatsDash>
+           <StatsDisclaimer/>
+        </div>
       )}
     </div>
   );
 };
 export default ImagesStatsModal;
+
