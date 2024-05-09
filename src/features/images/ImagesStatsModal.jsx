2630f52e000031ff5c52a5bde00c2be91d8f1c01
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2024-05-09 16:20:57 -0700

    Code review updates

--- src/features/images/ImagesStatsModal.jsx
+++ src/features/images/ImagesStatsModal.jsx
@@ -10,10 +10,9 @@ import {
 import { selectActiveFilters } from '../filters/filtersSlice.js';
 import { SimpleSpinner, SpinnerOverlay } from '../../components/Spinner';
 import NoneFoundAlert from '../../components/NoneFoundAlert';
-import {SelectedCount} from '../../components/Accordion.jsx';
+import { SelectedCount } from '../../components/Accordion.jsx';
 import { indigo } from '@radix-ui/colors';
 import InfoIcon from '../../components/InfoIcon';
-import Warning from '../../components/Warning';
 import {
   Bar,
   XAxis,
@@ -22,7 +21,7 @@ import {
   Tooltip,
   Legend,
   BarChart,
-  ResponsiveContainer
+  ResponsiveContainer,
 } from 'recharts';
 
 const StatsDash = styled('div', {
@@ -33,216 +32,184 @@ const StatsDash = styled('div', {
   borderRadius: '5px',
   padding: '10px',
   gap: '10px',
-  minWidth: '660px'
+  minWidth: '660px',
 });
 
-let reviewedCount = "The total number of images that are edited in some way. Note that this does not include " +
-"images which a user invalidated all labels on all objects, but didn't mark it empty. Because multiple users can edit the same image, the sum of all reviewers \"Reviewed Counts\" very likely will not equal the \"Reviewed Count\"."
+const reviewedCount =
+  'The total number of images that are edited in some way. Note that this does not include ' +
+  'images which a user invalidated all labels on all objects, but didn\'t mark it empty. Because multiple users can edit the same image, the sum of all reviewers "Reviewed Counts" very likely will not equal the "Reviewed Count".';
 
-let reviewerList = "Each reviewer's \"Reviewed Count\" is the total number of images they have edited in some way (validated/invalidated a label, added objects, etc.). "+
-"Because multiple users can edit the same image, and because images that have been edited can still be considered \"not reviewed\" (e.g., if a user invalidated all labels on all objects, but did't mark it empty), the sum of all reviewers \"Reviewed Counts\" very likely will not equal the \"Reviewed Count\"."
+const reviewerList =
+  'Each reviewer\'s "Reviewed Count" is the total number of images they have edited in some way (validated/invalidated a label, added objects, etc.). ' +
+  'Because multiple users can edit the same image, and because images that have been edited can still be considered "not reviewed" (e.g., if a user invalidated all labels on all objects, but did\'t mark it empty), the sum of all reviewers "Reviewed Counts" very likely will not equal the "Reviewed Count".';
 
-let labelList= "The quantities in the \"Label List\" are for locked objects with validated labels only, so they do not include ML predicted labels that need review."
+const labelList =
+  'The quantities in the "Label List" are for locked objects with validated labels only, so they do not include ML predicted labels that need review.';
 
-let multiReviewerCount = "Total number of images with multiple reviewers."
-let imagesCount= "Total number of images."
+const multiReviewerCount = 'Total number of images with multiple reviewers.';
+const imagesCount = 'Total number of images. test';
 
-
-const Heading = ({label, content}) => {
+const Heading = ({ label, content }) => {
   const Content = styled('div', {
     maxWidth: '300px',
-    lineHeight: '17px'
+    lineHeight: '17px',
   });
-  return(
-    <div style={{display:"flex", flexDirection:"row"}}>
-       <label >{label}
-         <InfoIcon tooltipContent={<Content>{content}</Content>} />
-       </label>
-    </div>
-  )
-}
-
-let mappings = {};
-mappings = {imageCount: "Image Count", reviewedCount: "Reviewed Count",
-  reviewerList: "Reviewer List", notReviewed: "Not Reviewed", labelList: "Label List",
-  multiReviewerCount: "Multi Reviewer Count", userId: "User", reviewed: "Reviewed"};
-
-function mapLabel(s) {
-  return (mappings[s] == undefined) ? (s + ":") : mappings[s];
-}
-
-function changeCard(color) {
-  const StatsCard = styled('div', {
-    background: color,
-    padding: '15px',
-    borderRadius: '5px',
-    width: '100%',
-    height: '100%',
-    overflowX: 'scroll',
-    overflowY: 'scroll'
-  });
-  return StatsCard;
-}
-
-const StatsItem = ({name, stat}) => {
-  switch (name) {
-    case "imageCount": {
-      return (
-        <RatioCard label={mapLabel(name)} tnum={stat} bnum={null} content={imagesCount}></RatioCard>
-      );
-    }
-    case "reviewedCount": {
-      return (
-        <RatioCard label={mapLabel(name)} tnum={stat['reviewed']} bnum={stat['notReviewed']}
-        content={reviewedCount}></RatioCard>
-      );
-    }
-    case "reviewerList": {
-      return (
-        <ListCard label={mapLabel(name)} list={stat} content={reviewerList}></ListCard>
-      );
-    }
-    case "labelList": {
-      return (
-        <GraphCard label={mapLabel(name)} list={stat} content={labelList}></GraphCard>
-      );
-    }
-    case "multiReviewerCount": {
-      return (
-        <RatioCard label={mapLabel(name)} tnum={stat} bnum={null} content={multiReviewerCount}></RatioCard>
-      );
-    }
-  }
   return (
-    <Warning>
-      Error: There is nothing to display for the statistic &quot;{name}&quot;
-    </Warning>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <label>
+        {label}
+        <InfoIcon tooltipContent={<Content>{content}</Content>} />
+      </label>
+    </div>
   );
 };
 
-const RatioCard = ({label, tnum, bnum, content}) => {
-  const StatsCard = changeCard('white')
-  const Container = styled('div', {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  })
-  const NumerContainer = styled('div', {
-    width: '100%',
-    marginTop: '15px'
-  })
-  const LargeNumber = styled(SelectedCount, {
-    fontSize: '30px',
-    width: '100%',
-  })
-  const SmallNumber = styled('div', {
-    width: '100%',
-    textAlign: 'right',
-    alignSelf: 'flex-end'
-  })
+const mappings = {
+  imageCount: 'Image Count',
+  reviewedCount: 'Reviewed Count',
+  reviewerList: 'Reviewer List',
+  notReviewed: 'Not Reviewed',
+  labelList: 'Label List',
+  multiReviewerCount: 'Multi Reviewer Count',
+  userId: 'User',
+  reviewed: 'Reviewed',
+};
+
+function mapLabel(s) {
+  return mappings[s] == undefined ? s + ':' : mappings[s];
+}
+
+const StatsCard = styled('div', {
+  background: '$loContrast',
+  padding: '15px',
+  borderRadius: '5px',
+  width: '100%',
+  height: '100%',
+  overflowX: 'scroll',
+  overflowY: 'scroll',
+});
+
+const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+});
+const NumerContainer = styled('div', {
+  width: '100%',
+  marginTop: '15px',
+});
+const LargeNumber = styled(SelectedCount, {
+  fontSize: '30px',
+  width: '100%',
+});
+const SmallNumber = styled('div', {
+  width: '100%',
+  textAlign: 'right',
+  alignSelf: 'flex-end',
+});
+
+const RatioCard = ({ label, tnum, bnum, content }) => {
   return (
     <StatsCard>
-      <Heading content={content} label={label}/>
+      <Heading content={content} label={label} />
       <Container>
         <NumerContainer>
           <LargeNumber>{tnum}</LargeNumber>
         </NumerContainer>
-        {bnum !== null && 
-        <SmallNumber>/ {bnum}</SmallNumber>}
+        {bnum && <SmallNumber>/ {bnum}</SmallNumber>}
       </Container>
     </StatsCard>
   );
-}
+};
 
-const ListCard = ({label, list, content}) => {
-  const StatsCard = changeCard('white')
-  const Table = styled('div', {
-    display: 'flex',
-    flexDirection: 'column',
-    overflow: 'scroll',
-    borderRadius: '5px',
-    border: '1px solid $border',
-    maxHeight: '102px'
-  });
-  const Row = styled('div', {
-    display: 'flex',
-    flexDirection: 'row',
-    maxHeight: '50px',
-  });
-  const TableCell = styled('div', {
-    borderBottom: '1px solid $border',
-    textAlign: 'left',
-    minWidth: '200px',
-    width: '100%',
-    marginRight: '1px',
-    padding: '$2',
-    overflowX: 'scroll'
-  });
+const Table = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  overflow: 'scroll',
+  borderRadius: '5px',
+  border: '1px solid $border',
+  maxHeight: '102px',
+});
+
+const Row = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  maxHeight: '50px',
+});
+
+const TableCell = styled('div', {
+  borderBottom: '1px solid $border',
+  textAlign: 'left',
+  minWidth: '200px',
+  width: '100%',
+  marginRight: '1px',
+  padding: '$2',
+  overflowX: 'scroll',
+});
+
+const ListCard = ({ label, list, content }) => {
   const Header = () => {
-    let headers = list[0]
-    return(
+    let headers = list[0];
+    return (
       <Row>
         {Object.keys(headers).map((keyName, i) => (
-            <TableCell key={i} css={{background: '$backgroundDark'}}>{mapLabel(keyName)}</TableCell>
-          ))
-        } 
+          <TableCell key={i} css={{ background: '$backgroundDark' }}>
+            {mapLabel(keyName)}
+          </TableCell>
+        ))}
       </Row>
     );
-  }
+  };
   return (
     <StatsCard>
-      <Heading content={content} label={label}/>
-       <Table>
-       <Header/>
+      <Heading content={content} label={label} />
+      <Table>
+        <Header />
         {list.map((stat, index) => (
           <Row key={index}>
             {Object.keys(stat).map((keyName, i) => (
-                <TableCell key={i}>{stat[keyName]}</TableCell>
-              ))
-            } 
+              <TableCell key={i}>{stat[keyName]}</TableCell>
+            ))}
           </Row>
         ))}
-       </Table>
+      </Table>
     </StatsCard>
   );
-}
+};
 
-const GraphCard = ({label, list, content}) => {
-  const StatsCard = changeCard('white')
-  let width = 0
-  const data = []
+const GraphCard = ({ label, list, content }) => {
+  let width = 0;
+  const data = [];
   for (const [objkey, objval] of Object.entries(list)) {
-    data.push({"name": objkey, "Total Labels": objval})
-    width = Math.max(width, objkey.length)
+    data.push({ name: objkey, 'Total Labels': objval });
+    width = Math.max(width, objkey.length);
   }
-  width = (width - 6) * 9 + 60 
-  data.sort(function(a , b){return b["Total Labels"] - a["Total Labels"]})
+  width = (width - 6) * 9 + 60;
+  data.sort(function (a, b) {
+    return b['Total Labels'] - a['Total Labels'];
+  });
   function GetHeight() {
-    return 250 + ((data.length > 12) ? ((data.length - 12) * 20) : 0);
+    return 250 + (data.length > 12 ? (data.length - 12) * 20 : 0);
   }
 
   return (
     <StatsCard>
-      <Heading content={content} label={label}/>
-      <div style={{overflowY: 'scroll', overflowX: 'scroll'}}>
+      <Heading content={content} label={label} />
+      <div style={{ overflowY: 'scroll', overflowX: 'scroll' }}>
         <ResponsiveContainer width="100%" height={GetHeight()}>
-          <BarChart
-            layout="vertical"
-            data={data}
-          >
+          <BarChart layout="vertical" data={data}>
             <CartesianGrid stroke="#f5f5f5" />
-            <XAxis type="number" allowDecimals={false}/>
+            <XAxis type="number" allowDecimals={false} />
             <YAxis width={width} dataKey="name" type="category" scale="auto" interval={0} />
             <Tooltip />
             <Legend />
-            <Bar dataKey="Total Labels"  fill={indigo.indigo11}/>
+            <Bar dataKey="Total Labels" fill={indigo.indigo11} />
           </BarChart>
         </ResponsiveContainer>
       </div>
     </StatsCard>
-    
   );
-}
+};
 
 const ImagesStatsModal = ({ open }) => {
   const dispatch = useDispatch();
@@ -256,10 +223,8 @@ const ImagesStatsModal = ({ open }) => {
     const noErrors = !errors || errors.length === 0;
     if (open && stats === null && !noneFound && !isLoading && noErrors) {
       dispatch(fetchStats(filters));
-      
     }
   }, [open, stats, imagesStatsLoading, filters, dispatch]);
-
 
   useEffect(() => {
     const getStatsPending = imagesStatsLoading.isLoading && imagesStatsLoading.taskId;
@@ -281,27 +246,36 @@ const ImagesStatsModal = ({ open }) => {
         </NoneFoundAlert>
       )}
       {stats && (
-        <div>
-           <StatsDash>
-              <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
-                <StatsItem  name={"imageCount"} stat={stats["imageCount"]}></StatsItem>
-                <StatsItem  name={"reviewedCount"} stat={stats["reviewedCount"]}></StatsItem>
-                <StatsItem  name={"multiReviewerCount"} stat={stats["multiReviewerCount"]}></StatsItem>
-              </div>
-
-              {Object.keys(stats["labelList"]).length !== 0 && 
-                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                  <StatsItem name={"labelList"} stat={stats["labelList"]} />
-                </div>
-              }
-              
-              {stats["reviewerList"].length !== 0 &&
-                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                  <StatsItem name={"reviewerList"} stat={stats["reviewerList"]} />
-                </div>
-              }
-          </StatsDash>
-        </div>
+        <StatsDash>
+          <div style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
+            <RatioCard label={'imageCount'} tnum={stats.imageCount} content={imagesCount} />
+            <RatioCard
+              label={mapLabel('reviewedCount')}
+              tnum={stats.reviewedCount.reviewed}
+              bnum={stats.reviewedCount.notReviewed}
+              content={reviewedCount}
+            />
+            <RatioCard
+              label={mapLabel('multiReviewerCount')}
+              tnum={stats.multiReviewerCount}
+              content={multiReviewerCount}
+            />
+          </div>
+          {Object.keys(stats['labelList']).length !== 0 && (
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+              <GraphCard label={mapLabel('labelList')} list={stats.labelList} content={labelList} />
+            </div>
+          )}
+          {stats['reviewerList'].length !== 0 && (
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+              <ListCard
+                label={mapLabel('reviewerList')}
+                list={stats.reviewerList}
+                content={reviewerList}
+              ></ListCard>
+            </div>
+          )}
+        </StatsDash>
       )}
     </div>
   );
