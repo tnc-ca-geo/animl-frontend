f6ad68aea0605993546b9e611c4b64b14d7a82d2
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2024-12-05 14:05:24 -0800

    fix: if deleting image from loupe dropdown, set selected images to currently focused image

--- src/features/loupe/LoupeDropdown.jsx
+++ src/features/loupe/LoupeDropdown.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { styled } from '../../theme/stitches.config';
 import {
   DropdownMenu,
@@ -12,6 +12,7 @@ import IconButton from '../../components/IconButton.jsx';
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import DeleteImagesAlert from '../images/DeleteImagesAlert.jsx';
 import { setDeleteImagesAlertStatus } from '../images/imagesSlice';
+import { selectFocusIndex, setSelectedImageIndices } from '../review/reviewSlice.js';
 
 const StyledDropdownMenuTrigger = styled(DropdownMenuTrigger, {
   position: 'absolute',
@@ -21,8 +22,10 @@ const StyledDropdownMenuTrigger = styled(DropdownMenuTrigger, {
 
 const LoupeDropdown = ({ image }) => {
   const dispatch = useDispatch();
+  const focusIndex = useSelector(selectFocusIndex);
 
   const handleDeleteImageItemClick = () => {
+    dispatch(setSelectedImageIndices([focusIndex.image]));
     dispatch(setDeleteImagesAlertStatus({ openStatus: true, deleteImagesByFilter: false }));
   };
 
