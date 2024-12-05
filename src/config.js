2df6427454dcef98839502465568c8d6e6374777
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2024-12-05 13:39:46 -0800

    feat(227): enforce deletion limits

--- src/config.js
+++ src/config.js
@@ -16,8 +16,9 @@ const stage = import.meta.env.VITE_STAGE || process.env.NODE_ENV;
 export const API_URL = API_URLS[stage];
 export const IMAGES_URL = IMAGES_URLS[stage];
 export const IMAGE_QUERY_LIMITS = [10, 50, 100];
-export const IMAGE_DELETE_LIMIT = 100;
-
+export const SYNC_IMAGE_DELETE_LIMIT = 300; // when deleting w/o using task handler
+export const ASYNC_IMAGE_DELETE_BY_ID_LIMIT = 4000; // when deleting using task handler (by _id). Constrained by POST request size limits
+export const ASYNC_IMAGE_DELETE_BY_FILTER_LIMIT = 200000; // when deleting using task handler (by filter). Constrained by task Lambda timeout
 
 export const SUPPORTED_WIRELESS_CAMS = ['BuckEyeCam', 'RidgeTec', 'CUDDEBACK', 'RECONYX'];
 
--- src/features/loupe/DeleteImagesAlert.jsx
+++ src/features/loupe/DeleteImagesAlert.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   deleteImages,
-  selectImagesCount,
   selectImagesCountLoading,
   selectDeleteImagesAlertState,
   setDeleteImagesAlertStatus,
   selectImagesLoading,
+  selectImagesCount,
 } from '../images/imagesSlice.js';
 import { selectActiveFilters } from '../filters/filtersSlice.js';
 import { selectSelectedImages } from '../review/reviewSlice.js';
@@ -20,7 +20,11 @@ import {
 import Button from '../../components/Button.jsx';
 import { red } from '@radix-ui/colors';
 import { deleteImagesTask, fetchTask, selectDeleteImagesLoading } from '../tasks/tasksSlice.js';
-import { IMAGE_DELETE_LIMIT } from '../../config.js';
+import {
+  SYNC_IMAGE_DELETE_LIMIT,
+  ASYNC_IMAGE_DELETE_BY_ID_LIMIT,
+  ASYNC_IMAGE_DELETE_BY_FILTER_LIMIT,
+} from '../../config.js';
 import { SimpleSpinner, SpinnerOverlay } from '../../components/Spinner.jsx';
 
 const DeleteImagesAlert = () => {
@@ -44,9 +48,11 @@ const DeleteImagesAlert = () => {
 
   const handleConfirmDelete = () => {
     if (alertState.deleteImagesAlertByFilter) {
+      // if deleting by filter, always delete using task handler
       dispatch(deleteImagesTask({ imageIds: [], filters: filters }));
     } else {
-      if (selectedImages.length > IMAGE_DELETE_LIMIT) {
+      // if deleting by selection of IDs, use task handler if over limit
+      if (selectedImages.length > SYNC_IMAGE_DELETE_LIMIT) {
         dispatch(deleteImagesTask({ imageIds: selectedImageIds, filters: null }));
       } else {
         dispatch(deleteImages(selectedImageIds));
@@ -58,13 +64,65 @@ const DeleteImagesAlert = () => {
     dispatch(setDeleteImagesAlertStatus({ openStatus: false }));
   };
 
+  const deleteByIdLimitExceeded =
+    !alertState.deleteImagesAlertByFilter && selectedImages.length > ASYNC_IMAGE_DELETE_BY_ID_LIMIT;
+  const byFilterLimitExceeded =
+    alertState.deleteImagesAlertByFilter && imageCount > ASYNC_IMAGE_DELETE_BY_FILTER_LIMIT;
+
   const isSpinnerActive =
     (deleteImagesTaskLoading.isLoading && deleteImagesTaskLoading.taskId) ||
     (alertState.deleteImagesAlertByFilter && imageCountIsLoading.isLoading) ||
     imagesLoading.isLoading;
 
-  const filterText = `Are you sure you'd like to delete ${imageCount === 1 ? 'this image' : `these ${imageCount && imageCount.toLocaleString()} images`}?`;
-  const selectionText = `Are you sure you'd like to delete ${selectedImages.length === 1 ? 'this image' : `these ${selectedImages && selectedImages.length.toLocaleString()} images`}?`;
+  const filterTitle = `Are you sure you'd like to delete ${imageCount === 1 ? 'this image' : `these ${imageCount && imageCount.toLocaleString()} images`}?`;
+  const selectionTitle = `Are you sure you'd like to delete ${selectedImages.length === 1 ? 'this image' : `these ${selectedImages && selectedImages.length.toLocaleString()} images`}?`;
+  const defaultText = (
+    <div>
+      <p>This action can not be undone.</p>
+    </div>
+  );
+
+  const deleteByIdLimitAlertTitle = 'Delete Limit Exceeded';
+  const deleteByIdLimitAlertText = (
+    <div>
+      <p>
+        You have selected {selectedImages.length.toLocaleString()} images, which is more than the{' '}
+        {ASYNC_IMAGE_DELETE_BY_ID_LIMIT.toLocaleString()} image limit Animl supports when deleting
+        individually-selected images.
+      </p>
+      {/*TODO: Add a link to the documentation for more information on how to delete images.*/}
+      <p>
+        Please select fewer images, or use the delete-by-filter option, which can accommodate
+        deleting up to {ASYNC_IMAGE_DELETE_BY_FILTER_LIMIT.toLocaleString()} images at a time.
+      </p>
+    </div>
+  );
+
+  const deleteByFilterLimitAlertTitle = 'Delete Limit Exceeded';
+  const deleteByFilterLimitAlertText = (
+    <div>
+      <p>
+        There are {imageCount.toLocaleString()} images that match the currently selected filters,
+        which is more than the {ASYNC_IMAGE_DELETE_BY_FILTER_LIMIT.toLocaleString()} image limit
+        Animl supports when deleting images by filter.
+      </p>
+      <p>
+        To delete all of the currently matching images, you may need to apply additional filters to
+        stay within the limit and perform multiple separate deletion requests.
+      </p>
+    </div>
+  );
+
+  let title = alertState.deleteImagesAlertByFilter ? filterTitle : selectionTitle;
+  let text = defaultText;
+  if (deleteByIdLimitExceeded) {
+    title = deleteByIdLimitAlertTitle;
+    text = deleteByIdLimitAlertText;
+  } else if (byFilterLimitExceeded) {
+    title = deleteByFilterLimitAlertTitle;
+    text = deleteByFilterLimitAlertText;
+  }
+
   return (
     <Alert open={alertState.deleteImagesAlertOpen}>
       <AlertPortal>
@@ -75,16 +133,15 @@ const DeleteImagesAlert = () => {
               <SimpleSpinner />
             </SpinnerOverlay>
           )}
-          <AlertTitle>
-            {alertState.deleteImagesAlertByFilter ? filterText : selectionText}
-          </AlertTitle>
-          <p>This action can not be undone.</p>
+          <AlertTitle>{title}</AlertTitle>
+          {text}
           <div style={{ display: 'flex', gap: 25, justifyContent: 'flex-end' }}>
             <Button size="small" css={{ border: 'none' }} onClick={handleCancelDelete}>
               Cancel
             </Button>
             <Button
               size="small"
+              disabled={deleteByIdLimitExceeded || byFilterLimitExceeded}
               css={{
                 backgroundColor: red.red4,
                 color: red.red11,
