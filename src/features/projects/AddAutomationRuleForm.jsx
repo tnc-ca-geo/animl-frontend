7217c994c6a6189862ff8bba941c749e98da7201
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2025-05-06 20:55:37 -0700

    feat(310): add text filter to automation rule category config form

--- src/features/projects/AddAutomationRuleForm.jsx
+++ src/features/projects/AddAutomationRuleForm.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { styled } from '../../theme/stitches.config.js';
 import _ from 'lodash';
 import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
@@ -12,8 +13,15 @@ import {
   ButtonRow,
   FormFieldWrapper,
   FormError,
+  StandAloneInput as Input,
 } from '../../components/Form.jsx';
 import CategoryConfigForm from './CategoryConfigForm.jsx';
+
+const CategoryConfigFilter = styled('div', {
+  display: 'flex',
+  marginBottom: '$3',
+  width: '300px',
+});
 
 const emptyRule = {
   name: '',
@@ -105,6 +113,9 @@ const AddAutomationRuleForm = ({ project, availableModels, hideAddRuleForm, rule
 
   // discard rule
   const handleDiscardRuleClick = () => hideAddRuleForm();
+
+  // filter categories
+  const [categoryFilter, setCategoryFilter] = useState('');
 
   return (
     <FormWrapper>
@@ -229,7 +240,15 @@ const AddAutomationRuleForm = ({ project, availableModels, hideAddRuleForm, rule
             {values.action.categoryConfig &&
               Object.entries(values.action.categoryConfig).length > 0 && (
                 <div>
-                  <label>Confidence thresholds</label>
+                  <label htmlFor="event-label">Labels</label>
+                  <CategoryConfigFilter>
+                    <Input
+                      css={{ width: 320, height: 40, padding: '$0 $3', marginRight: '$3' }}
+                      placeholder="Filter by common name..."
+                      value={categoryFilter}
+                      onChange={(e) => setCategoryFilter(e.target.value)}
+                    />
+                  </CategoryConfigFilter>
                   <FieldArray name="categoryConfigs">
                     <>
                       {Object.entries(values.action.categoryConfig)
@@ -237,6 +256,13 @@ const AddAutomationRuleForm = ({ project, availableModels, hideAddRuleForm, rule
                           ([k]) =>
                             !(values.action.model.value.includes('megadetector') && k === 'empty'),
                         ) // NOTE: manually hiding "empty" categories b/c it isn't a real category returned by MDv5
+                        .filter(([k]) => {
+                          if (categoryFilter) {
+                            return k.toLowerCase().includes(categoryFilter.toLowerCase());
+                          } else {
+                            return true;
+                          }
+                        })
                         .map(([k, v]) => (
                           <CategoryConfigForm key={k} catName={k} config={v} />
                         ))}
